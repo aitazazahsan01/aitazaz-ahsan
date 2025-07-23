3ab0437b9b633041dd8b4bebf6fb1c5a66a4844f
Muhammad Aitazaz Ahsan <152264393+aitazazahsan01@users.noreply.github.com>
2025-07-23 11:03:54 -0700

    Update ProjectsSection.tsx

--- aitazazahsan/src/app/components/ProjectsSection.tsx
+++ aitazazahsan/src/app/components/ProjectsSection.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { motion } from 'framer-motion';
-import { Github, ArrowUpRight } from 'lucide-react';
+import { Github } from 'lucide-react';
 import Image from 'next/image';
 
 // Your project data
